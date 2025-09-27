0f27b2f313e50fa063066a984c10ef28f941c891
DAQEM <daqemyt@gmail.com>
2025-09-27 14:57:44 +0200

    Create multiple instances

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,148 +1,202 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import type * as DocsPlugin from "@docusaurus/plugin-content-docs";
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+    title: "DAQEM Studios",
+    tagline: "Dive into a world of wonder.",
+    favicon: "img/favicon.ico",
+    future: {
+        v4: true,
+    },
+    url: "https://daqem.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    markdown: {
+        hooks: {
+            onBrokenMarkdownLinks: "warn",
+            onBrokenMarkdownImages: "warn",
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
+    },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+    presets: [
+        [
+            "classic",
+            {
+                docs: false,
+                blog: {
+                    showReadingTime: true,
+                    feedOptions: {
+                        type: ["rss", "atom"],
+                        xslt: true,
+                    },
+                    onInlineTags: "warn",
+                    onInlineAuthors: "warn",
+                    onUntruncatedBlogPosts: "warn",
+                },
+                theme: {
+                    customCss: "./src/css/custom.css",
+                },
+            } satisfies Preset.Options,
+        ],
     ],
-  ],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "DAQEM Studios",
+            logo: {
+                alt: "DAQEM Studios Logo",
+                src: "img/logo.svg",
+            },
+            items: [
+                { to: "/projects", label: "Projects", position: "left" },
+                { to: "/blog", label: "Blog", position: "left" },
+                {
+                    href: "https://github.com/facebook/docusaurus",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "Docs",
+                    items: [
+                        {
+                            label: "Jobs+",
+                            to: "/projects/jobsplus/wiki",
+                        },
+                        {
+                            label: "YAML Config",
+                            to: "/projects/yaml-config/wiki",
+                        },
+                        {
+                            label: "UI Lib",
+                            to: "/projects/ui-lib/wiki",
+                        },
+                        {
+                            label: "Arc Lib",
+                            to: "/projects/arc/wiki",
+                        },
+                        {
+                            label: "Item Restrictions",
+                            to: "/projects/item-restrictions/wiki",
+                        },
+                        {
+                            label: "More Projects",
+                            to: "/projects",
+                        },
+                    ],
+                },
+                {
+                    title: "Community",
+                    items: [
+                        {
+                            label: "Discord",
+                            href: "https://daqem.com/discord",
+                        },
+                        {
+                            label: "CurseForge",
+                            href: "https://www.curseforge.com/members/daqem",
+                        },
+                        {
+                            label: "Modrinth",
+                            href: "https://modrinth.com/organization/daqem-studios",
+                        },
+                        {
+                            label: "Ko-fi",
+                            href: "https://ko-fi.com/daqem",
+                        },
+                    ],
+                },
+                {
+                    title: "More",
+                    items: [
+                        {
+                            label: "Blog",
+                            to: "/blog",
+                        },
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/DAQEM",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} DAQEM Studios. All rights reserved.`,
         },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
         },
-        {
-          title: 'Community',
-          items: [
+    } satisfies Preset.ThemeConfig,
+    plugins: [
+        [
+            "@docusaurus/plugin-content-docs",
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
+                id: "jobsplus",
+                path: "docs/jobsplus",
+                routeBasePath: "/projects/jobsplus/wiki",
+                sidebarPath: "./sidebars.ts",
+                sidebarCollapsed: true,
+                editUrl: "https://github.com/DAQEM/website/tree/main/",
+            } satisfies Partial<DocsPlugin.PluginOptions>,
+        ],
+        [
+            "@docusaurus/plugin-content-docs",
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
+                id: "yaml-config",
+                path: "docs/yaml-config",
+                routeBasePath: "/projects/yaml-config/wiki",
+                sidebarPath: "./sidebars.ts",
+                sidebarCollapsed: true,
+                editUrl: "https://github.com/DAQEM/website/tree/main/",
+            } satisfies Partial<DocsPlugin.PluginOptions>,
+        ],
+        [
+            "@docusaurus/plugin-content-docs",
             {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
+                id: "ui-lib",
+                path: "docs/ui-lib",
+                routeBasePath: "/projects/ui-lib/wiki",
+                sidebarPath: "./sidebars.ts",
+                sidebarCollapsed: true,
+                editUrl: "https://github.com/DAQEM/website/tree/main/",
+            } satisfies Partial<DocsPlugin.PluginOptions>,
+        ],
+        [
+            "@docusaurus/plugin-content-docs",
             {
-              label: 'Blog',
-              to: '/blog',
-            },
+                id: "arc",
+                path: "docs/arc",
+                routeBasePath: "/projects/arc/wiki",
+                sidebarPath: "./sidebars.ts",
+                sidebarCollapsed: true,
+                editUrl: "https://github.com/DAQEM/website/tree/main/",
+            } satisfies Partial<DocsPlugin.PluginOptions>,
+        ],
+        [
+            "@docusaurus/plugin-content-docs",
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+                id: "item-restrictions",
+                path: "docs/item-restrictions",
+                routeBasePath: "/projects/item-restrictions/wiki",
+                sidebarPath: "./sidebars.ts",
+                sidebarCollapsed: true,
+                editUrl: "https://github.com/DAQEM/website/tree/main/",
+            } satisfies Partial<DocsPlugin.PluginOptions>,
+        ],
+    ],
 };
 
 export default config;
