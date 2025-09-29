5327601c16e41bba3abcd79623339b24aa13df6a
DAQEM <daqemyt@gmail.com>
2025-09-29 22:57:07 +0200

    fix: removed projects page

--- src/pages/projects/index.tsx
+++
@@ -1,8 +0,0 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import type { ReactNode } from "react";
-
-export default function Home(): ReactNode {
-    const { siteConfig } = useDocusaurusContext();
-    return <Layout></Layout>;
-}
